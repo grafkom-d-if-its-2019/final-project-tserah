ca8fe0a3594bf4c4393d58461572205b18f0058f
Setya Wibawa <durianpeople@gmail.com>
2019-12-01 18:58:52 +0700

    update

---
+++ Frontend/index.js
@@ -0,0 +1,5 @@
+var scene = new THREE.Scene();
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
