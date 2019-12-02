72a9f860047e7be7e730d0e00551797560b402cd
Setya Wibawa <durianpeople@gmail.com>
2019-12-02 14:28:47 +0700

    update

--- Frontend/index.js
+++ Frontend/index.js
@@ -11,4 +11,7 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 5;
+
 var testDrawable = new Drawable(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 0x00ff00}));
