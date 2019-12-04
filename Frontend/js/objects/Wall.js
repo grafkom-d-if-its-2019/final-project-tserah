18981a9a8f2f64248a9e43881a416cdd5c195000
Dex <prdptlx@gmail.com>
2019-12-04 19:51:58 +0700

    For efficient mesh

--- Frontend/js/objects/Wall.js
+++ Frontend/js/objects/Wall.js
@@ -13,16 +13,16 @@ export default class Wall extends Drawable {
     constructor(width, height, faceAxis) {
         switch (faceAxis) {
             case 'X_AXIS':
-                super(new THREE.BoxGeometry(1, height, width), new THREE.MeshLambertMaterial({ color: 0xffffff }));
+                super(new THREE.BoxBufferGeometry(1, height, width), new THREE.MeshLambertMaterial({ color: 0xffffff }));
                 break;
             case 'Y_AXIS':
-                super(new THREE.BoxGeometry(width, 1, height), new THREE.MeshLambertMaterial({ color: 0xffffff }));
+                super(new THREE.BoxBufferGeometry(width, 1, height), new THREE.MeshLambertMaterial({ color: 0xffffff }));
                 break;
             case 'Z_AXIS':
-                super(new THREE.BoxGeometry(width, height, 1), new THREE.MeshLambertMaterial({ color: 0xffffff }));
+                super(new THREE.BoxBufferGeometry(width, height, 1), new THREE.MeshLambertMaterial({ color: 0xffffff }));
                 break;
             default:
-                super(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshLambertMaterial({ color: 0xffffff}));
+                super(new THREE.BoxBufferGeometry(1, 1, 1), new THREE.MeshLambertMaterial({ color: 0xffffff}));
         }
     }
 
