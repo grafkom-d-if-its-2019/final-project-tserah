48c1caf1924337fd3e0b3f9df79dafecc28e17c6
durianpeople <durianpeople@gmail.com>
2019-12-13 17:09:43 +0700

    tambah tembok + pencahayaan

--- Frontend/js/objects/Wall.js
+++ Frontend/js/objects/Wall.js
@@ -15,13 +15,13 @@ export default class Wall extends Drawable {
         let texture = new THREE.TextureLoader().load('../assets/images.jpg');
         switch (faceAxis) {
             case X_AXIS:
-                super(new THREE.BoxBufferGeometry(.1, height, width), new THREE.MeshLambertMaterial({ color: 0xff0000 }));
+                super(new THREE.BoxBufferGeometry(.1, width, height), new THREE.MeshLambertMaterial({ color: 0xff0000 }));
                 break;
             case Y_AXIS:
-                super(new THREE.BoxBufferGeometry(width, .1, height), new THREE.MeshLambertMaterial({ color: 0xff0000 }));
+                super(new THREE.BoxBufferGeometry(width, .1, height), new THREE.MeshLambertMaterial({ color: 0x00ff00 }));
                 break;
             case Z_AXIS:
-                super(new THREE.BoxBufferGeometry(width, height, .1), new THREE.MeshLambertMaterial({ color: 0xff0000 }));
+                super(new THREE.BoxBufferGeometry(width, height, .1), new THREE.MeshLambertMaterial({ color: 0x0000ff }));
                 break;
             default:
                 super(new THREE.BoxBufferGeometry(.1, .1, .1), new THREE.MeshLambertMaterial({ color: 0xff0000}));
