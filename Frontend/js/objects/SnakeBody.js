e0a78b836c6c900f0adf09ec9bcaedfe2ba39323
bagasys <bagasys3@gmail.com>
2019-12-14 16:17:58 +0700

    ular nabrak tembok

--- Frontend/js/objects/SnakeBody.js
+++ Frontend/js/objects/SnakeBody.js
@@ -55,7 +55,7 @@ class SnakeBody extends Drawable {
             }
         }
         else if (drawable instanceof Wall) {
-            console.log("nabrak tembok");
+            this.snake.onCollideWithWall();
         }
     }
 
