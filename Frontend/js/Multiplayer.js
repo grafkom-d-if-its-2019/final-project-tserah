2e46a9ecb83a12eadfea402223b1f05be31c53c9
bagasys <bagasys3@gmail.com>
2019-12-14 20:34:30 +0700

    bug fixing snake tabrak snake
    bug fixing snake tabrak snake

--- Frontend/js/Multiplayer.js
+++ Frontend/js/Multiplayer.js
@@ -1,63 +1,77 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 import Handler from "./objects/Handler";
-import Viewport from './objects/Viewport';
-import Player from './objects/Player';
-import io from 'socket.io-client';
+import Viewport from "./objects/Viewport";
+import Player from "./objects/Player";
+import io from "socket.io-client";
 
 export default class Multiplayer {
+  /** @type {THREE.PerspectiveCamera} */
+  static overviewCamera;
+  /** @type {Player[]} */
+  static players;
+  /** @type {String} */
+  static name;
+  /** @type {Positioning} */
+  static position;
+  static init() {
+    // Default viewports
+    this.overviewCamera = new THREE.PerspectiveCamera(
+      90,
+      window.innerWidth / window.innerHeight,
+      0.5,
+      1000
+    );
+    this.overviewCamera.rotateX(-Math.PI / 2);
+    this.overviewCamera.position.y = 15;
+    Handler.registerViewport(new Viewport(0, 0.5, 1, 0.5, this.overviewCamera));
+    this.players = new Array();
 
-	/** @type {THREE.PerspectiveCamera} */
-	static overviewCamera;
-	/** @type {Player[]} */
-	static players;
-	/** @type {String} */
-	static name;
-	/** @type {Positioning} */
-	static position;
-	static init() {
-		// Default viewports
-		this.overviewCamera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.5, 1000);
-		this.overviewCamera.rotateX(-Math.PI / 2);
-		this.overviewCamera.position.y = 15;
-		Handler.registerViewport(new Viewport(0, 0.5, 1, 0.5, this.overviewCamera));
-		this.players = new Array();
+    // Initialize socket
+    console.log("Connecting...");
+    this.socket = io("http://" + window.location.hostname + ":8000");
+    var local_socket = this.socket;
+    this.socket.on(
+      "connect",
+      function() {
+        console.log("Connected.", this.socket);
+        this.socket.emit("iamhost");
+      }.bind(this)
+    );
+    this.socket.on("log", function(emission) {
+      console.log("Server Log:", emission);
+    });
+    this.socket.on("new_player", function(request) {
+      console.log("Player " + request.name + " tries to join");
+      // this.name
+      Multiplayer.newPlayer(request.name);
+    });
+    this.socket.on("player_leave", function(id) {
+      console.log("Socket id " + id + " left");
+    });
+  }
+  /**
+   *
+   * @param {String} name
+   */
+  static newPlayer(name) {
+    this.name = name;
+    this.players[name] = new Player(name);
+    this.players[name].positioning.speed = 3;
+    this.position = this.players[name].positioning;
 
-		// Initialize socket
-		console.log("Connecting...");
-		this.socket = io('http://'+window.location.hostname+':8000');
-		var local_socket = this.socket;
-		this.socket.on('connect', (function () {
-			console.log("Connected.", this.socket);
-			this.socket.emit("iamhost");
-		}).bind(this));
-		this.socket.on('log', function (emission) {
-			console.log('Server Log:', emission);
-		});
-		this.socket.on('new_player', function(request){
-			console.log("Player " + request.name + ' tries to join');
-			// this.name
-			Multiplayer.newPlayer(request.name);
-		});
-		this.socket.on("player_leave", function(id){
-			console.log("Socket id " + id + " left");
-		});
-	}
-	/**
-	 * 
-	 * @param {String} name 
-	 */
-	static newPlayer(name) {
-		this.name = name;
-		this.players[name] = new Player(name);
-		this.players[name].positioning.speed = 3;
-		this.position = this.players[name].positioning;
-		
-		window.camera = this.players[name].camera;
-		window.player = this.players[name];
-		
-		Handler.registerViewport(new Viewport(0.5 * ((Object.keys(this.players).length - 1)), 0, 0.5, 0.5, this.players[name].camera));
+    window.camera = this.players[name].camera;
+    window.player = this.players[name];
 
-		Handler.generateFood();
-	}
+    Handler.registerViewport(
+      new Viewport(
+        0.5 * (Object.keys(this.players).length - 1),
+        0,
+        0.5,
+        0.5,
+        this.players[name].camera
+      )
+    );
 
+    Handler.generateFood();
+  }
 }
