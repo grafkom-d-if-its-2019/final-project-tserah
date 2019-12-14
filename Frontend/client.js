7273a4c8e55c1b7fb6edf26c2c764886b051f363
durianpeople <durianpeople@gmail.com>
2019-12-14 20:34:44 +0700

    Revert "handler collision optimization"
    This reverts commit 8f15d660c238f44c85d339b9dad9a4295e81a3eb.

--- Frontend/client.js
+++ Frontend/client.js
@@ -3,45 +3,42 @@ import io from 'socket.io-client';
 var socket = io('http://' + window.location.hostname + ':8000');
 var name;
 function leftController(e) {
-	console.log(name);
 	socket.emit('controller', {name: name, action:'left'});
 };
 
 function rightController(e){
-	console.log(name);
 	socket.emit('controller', {name: name, action:'right'});
 }
 
 function forwardController(e){
-	console.log(name);
 	socket.emit('controller', {name: name, action:'forward'});
 }
 
 function backwardController(e){
-	console.log(name);
 	socket.emit('controller', {name: name, action:'backward'});
 }
 
 function main() {
-
-	document.getElementById('joinbtn').onclick = function(){
-		name = document.getElementById("nameinput").value;
-		socket.emit("join", {name: name});
-	};
 	// Event emitters
 	document.getElementById('left').onclick = leftController;
 	document.getElementById('right').onclick = rightController;
 	document.getElementById('forward').onclick = forwardController;
 	// document.getElementById('backward').onclick = backwardController;
 
-	// console.log("Connecting...");
+	console.log("Connecting...");
 	
 	socket.on('connect', function () {
-		// console.log("Connected.", socket);
+		console.log("Connected.", socket);
+		name = prompt("Nama:");
+		socket.emit("join", {name: name});
 	});
 	socket.on('log', function (emission) {
-		// console.log('Server Log:', emission);
+		console.log('Server Log:', emission);
 	});
+
+	
+	
+	
 }
 
 main();
